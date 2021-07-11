4ffd0762c70dac31d28835de4e87c8b0e8f561b1
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-07-11 19:13:43 +0300

    Refactored the project to improve code quality and consistency

---
+++ MoviesRecommendationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoviesRecommendationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
